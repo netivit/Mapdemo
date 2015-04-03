f9182532ec024b19be97d69cbaebf254365ef51c
netivit <nativit_ton@hotmail.com>
2015-04-03 23:28:07 +0700

    Initial commit

---
+++ app/src/main/java/com/example/mapdemo/FragmentActivity.java
@@ -0,0 +1,12 @@
+package com.example.mapdemo;
+
+import android.os.Bundle;
+
+/**
+ * Created by admin on 3/4/2558.
+ */
+public class FragmentActivity {
+    public void onCreate(Bundle savedInstanceState) {
+
+    }
+}
